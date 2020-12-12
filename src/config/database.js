718a783216348cd3d79efb15b760f484c1c8f721
Marcos <dev.marcos1995@gmail.com>
2020-12-11 23:31:48 -0300

    sending to the production teste

--- src/config/database.js
+++ src/config/database.js
@@ -6,6 +6,7 @@ module.exports = {
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   dialect: process.env.DATABASE_DIALECT,
+  port: process.env.DATABASE_PORT,
   define: {
     timestamps: true,
   },
